2a77d5579775aac8daf390b8bc7a78785aa5678c
asantoshka <asantoshka@outlook.com>
2022-05-16 21:54:32 +0530

    First commit

---
+++ encoder.py
@@ -0,0 +1,39 @@
+import base64, hashlib
+from Crypto.Hash import MD4
+import urllib.parse
+
+hash_funcs = {'md5' : hashlib.md5,  'sha1' : hashlib.sha1, 'sa224' : hashlib.sha224, 'sha256' : hashlib.sha256, 'sha384' : hashlib.sha384, 'sha512' : hashlib.sha512, 'sha3_224' : hashlib.sha3_224, 'sha3_256' : hashlib.sha3_256, 'sha3_384' : hashlib.sha3_384, 'sha3_512' : hashlib.sha3_512}
+
+
+def encoder(ptext):
+    result = {}
+    result['base64'] = base64.standard_b64encode(ptext).decode("utf-8") 
+    result['base32'] = base64.b32encode(ptext).decode("utf-8") 
+    result['url'] = urllib.parse.quote(ptext)
+    result['hex'] = str(ptext).encode('utf-8').hex()
+    return result
+
+def hasher(ptext):
+    result = {}
+    result['md4'] = hashlib.new('md4',ptext).hexdigest()
+    for key in hash_funcs:
+        result[key] = hash_funcs[key](ptext).hexdigest()
+    return result
+
+def hash_file(filename, func):
+   h = func()
+   with open(filename,'rb') as file:
+       chunk = 0
+       while chunk != b'':
+           chunk = file.read(1024)
+           h.update(chunk)
+   return h.hexdigest()
+
+def hash_func_itr(filename):
+    result = {}
+    for key in hash_funcs:
+        func = hash_funcs[key]
+        result[key] = hash_file(filename=filename,func=func)
+    return result
+        
+
---
+++ main.py
@@ -0,0 +1,79 @@
+import typer
+import os
+from rich.console import Console
+from rich.table import Table
+import encoder
+
+console = Console()
+app = typer.Typer()
+
+def show(plainText, data):    
+    console.print(f"The plain text String: {plainText.decode('utf-8')}", style='bold blue')
+    table = Table(show_header=True, header_style="bold blue")
+    table.add_column("Type", style="dim", width=20)
+    table.add_column("Output", min_width=20)
+
+    for ops in data:
+        table.add_row(ops, data[ops])
+    
+    console.print(table)
+
+def stringCheck(string):
+    if string == "":    
+        plainText = bytes(input('Enter the string: '), 'utf-8')
+    else: 
+        plainText = bytes(string, 'utf-8')
+    return plainText
+
+def typeCheck(typ, output):
+    if typ == "":     
+        result = output
+    else:
+        result = {}
+        if typ in output.keys():
+            result[typ] = output[typ]
+        else: 
+            console.print("This type of Encoding or Hashing is not enabled in this tool.", style='bold red')
+            result["Invalid operation"] = "I am sorry"
+    return result
+
+def verifyFile(file):
+    return os.path.exists(file)
+
+encode_help = 'Performs various types of encoding e.g. Base64, Base32, URL etc.'
+@app.command(short_help=encode_help)
+def encode(string: str=typer.Option("","--string", "-s", help="Enter the string value. However it is optional to use.", show_default=False), 
+            type: str=typer.Option("","--type", "-t", help="Type of Encoding. If you don't know what type of hash to use. Simply don't use this option(--type)", show_default=False)):
+    plainText = stringCheck(string=string)
+    output = encoder.encoder(ptext=plainText)
+    result = typeCheck(typ=type, output=output)
+    show(plainText, result)
+
+enhash_help = 'Perfoms various types of hashings such as SHA, MD5 etc'
+@app.command(short_help=enhash_help)
+def enhash(string: str=typer.Option("","--string", "-s", help="Enter the string value. However it is optional to use.", show_default=False), 
+            file: str=typer.Option("","--file", "-f", help="Enter the file name", show_default=False),
+            type: str=typer.Option("","--type", "-t", help="Type of hash. If you don't know what type of hash to use. Simply don't use this option(--type)", show_default=False)):
+    if file != "":
+        if verifyFile(file):
+            output = encoder.hash_func_itr(file)
+            result = typeCheck(typ=type, output=output)
+            show(bytes(file, 'utf-8'), result)
+        else:
+            console.print(f"{file} : File doesn't exist.", style="bold red")
+    else:
+        plainText = stringCheck(string=string)
+        output = encoder.hasher(ptext=plainText)
+        result = typeCheck(typ=type, output=output)
+        show(plainText, result)
+
+if __name__ == '__main__':
+    app()
+
+
+
+
+
+
+
+
